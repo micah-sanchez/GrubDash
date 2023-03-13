bd03a69d0a7a9a6713bdcee9750d3ab502caa117
micah-sanchez <mjsanch4@gmail.com>
2023-03-12 23:57:18 -0600

    added middleware for get/list, get/read, post/create

--- src/dishes/dishes.controller.js
+++ src/dishes/dishes.controller.js
@@ -7,3 +7,62 @@ const dishes = require(path.resolve("src/data/dishes-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /dishes handlers needed to make the tests pass
+function bodyDataHas(propertyName) {
+    return function (req, res, next) {
+      const { data = {} } = req.body;
+      if (data[propertyName]) {
+        return next();
+      }
+      next({ status: 400, message: `Must include a ${propertyName}` });
+    };
+  }
+  
+  function validateDish(req, res, next) {
+    const { dishId } = req.params;
+    const foundDish = dishes.find((dish) => dish.id === Number(dishId))
+    
+    if (foundDish) {
+      return next();
+    }
+    next({
+      status: 400,
+      message: `Dish ID not found: ${dishId}`
+    })
+  }
+  
+  function list(req, res, next) {
+    res.json({data: dishes})
+  }
+  
+  function create(req, res, next) {
+    const { data: {name, description, price, image_url} = {} } = req.body;
+    const newDish = {
+      id: nextId,
+      name,
+      description,
+      price,
+      image_url,
+    }
+    dishes.push(newDish);
+    res.status(201).json({data: newDish})
+  }
+  
+  function read(req, res, next) {
+    const { dishId } = req.params;
+    const foundDish = dishes.find((dish) => dish.id === Number(dishId))
+    
+    res.json({data: foundDish});
+  }
+  
+  module.exports = {
+    list,
+    create: [
+      bodyDataHas("name"),
+      bodyDataHas("description"),
+      bodyDataHas("price"),
+      bodyDataHas("image_url"),
+      create,],
+    read: [
+      validateDish,
+      read,]
+  }
