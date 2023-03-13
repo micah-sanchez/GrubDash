02ce5203b980de68a5371d8fab6b185bbcea4453
micah-sanchez <mjsanch4@gmail.com>
2023-03-12 23:56:43 -0600

    added routes for get/list, get/read, post/create

--- src/dishes/dishes.router.js
+++ src/dishes/dishes.router.js
@@ -1,5 +1,12 @@
 const router = require("express").Router();
+const controller = require("./dishes.controller")
 
 // TODO: Implement the /dishes routes needed to make the tests pass
+router.route("/")
+    .get(controller.list)
+    .post(controller.create)
+
+router.route("/:dishId")
+    .get(controller.read)
 
 module.exports = router;
