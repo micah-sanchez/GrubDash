89a213a605ff208f147f9ad64ecfb56f092281b5
micah-sanchez <mjsanch4@gmail.com>
2023-03-13 20:06:35 -0600

    created list function

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -7,3 +7,10 @@ const orders = require(path.resolve("src/data/orders-data"));
 const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
+function list(req, res, next) {
+    res.json({ data: orders })
+}
+
+module.exports = {
+    list,
+}
